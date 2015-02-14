bc7868639440be47ddc87bd85fa7ad9eca81e0a0
bnguyen654 <bnguyen9469@yahoo.com>
2015-02-13 23:34:21 -0800

    Update Pulley.java

--- src/org/oastem/frc/control/Pulley.java
+++ src/org/oastem/frc/control/Pulley.java
@@ -2,23 +2,31 @@ package org.oastem.frc.control;
 import edu.wpi.first.wpilibj.CounterBase;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  * @author joyhsu0504
  * @author bnguyen654
  */
-/*
-public class Pulley(){
+
+public class Pulley{
     private static Pulley instance;
     
     private CANJaguar right, left;
+    boolean rRefl, lRefl;
     
-    private int codesPerRev, distPerRev;
+    private int codesPerRev = 497,
+    		distPerRev = 0; //to be checked
     
-    private final int MOVE_CONSTANT = 1/distPerRev;
+    //values are to be inches
+    private final double AUTO_MOVE_DIST = 1;
+    
+    private final double FORWARD_LIMIT = 1,
+    		REVERSE_LIMIT = 1;
     
     private Pulley(){
+    	
     }
     
     public static Pulley getInstance(){
@@ -29,10 +37,18 @@ public class Pulley(){
     }
     
     public void init(int rightId, int leftId, double p, double i, double d){
-        right = new CANJaguar(rightId);
-        right.setPositionMode(CANJaguar.kQuadEncoder, codesPerRev, p, i, ,d);
-        left = new CANJaguar(leftId);
+       init(new CANJaguar(rightId), new CANJaguar(leftId), p, i, d);
+    }
+    public void init(CANJaguar r, CANJaguar l, double p, double i, double d){
+        right = r;
+        right.setPositionMode(CANJaguar.kQuadEncoder, codesPerRev, p, i, d);
+        //right.configSoftPositionLimits(FORWARD_LIMIT, REVERSE_LIMIT);
+        //not sure
+        
+        left = l;
         left.setPositionMode(CANJaguar.kQuadEncoder, codesPerRev, p, i, d);
+        //left.configSoftPositionLimits(FORWARD_LIMIT, REVERSE_LIMIT);
+        //wat do
     }
     
     public void configEnc(int cpr, int dpr){
@@ -40,9 +56,34 @@ public class Pulley(){
         distPerRev = dpr;
     }
     
+    public void up(double dist){
+    	left.set(left.getPosition() +
+    			((lRefl ? -1 : 1) * i2r(dist)));
+    	right.set(right.getPosition() + 
+    			((rRefl ? -1 : 1) * i2r(dist)));
+    }
+    public void down(double dist){
+    	left.set(left.getPosition() -
+    			((lRefl ? -1 : 1) * i2r(dist)));
+    	right.set(right.getPosition() - 
+    			((rRefl ? -1 : 1) * i2r(dist)));
+    }
+    
+    public void up(){
+    	up(AUTO_MOVE_DIST);
+    }
+    
+    public void down(){
+    	down(AUTO_MOVE_DIST);
+    }
+    
     public void goToPos(double pos){
-        right.set(pos);
-        left.set(pos);
+        right.set(i2r(pos));
+        left.set(i2r(pos));
+    }
+    
+    public void debug(SmartDashboard dash){
+    	
     }
 
 	public boolean upToHook() {
@@ -56,5 +97,11 @@ public class Pulley(){
 	public boolean checkHooked() {
 		
 	}
+	private double r2i(double rev){ //rev to in
+		return rev * distPerRev;
+	}
+	private double i2r(double in){ //in to rev
+		return in/distPerRev;
+	}
     
-}*/
+}
